61d2fbca749c2dc40f5493f5f801208407381079
aOpenbrier <aopenbrier@gmail.com>
2019-06-17 19:55:57 -0700

    local server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.listen(PORT, () => console.log(`listening at ${PORT}`))

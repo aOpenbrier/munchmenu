6eb9994c1b017b29dc0ed8b23d928989f1982645
aOpenbrier <aopenbrier@gmail.com>
2019-06-25 16:21:42 -0700

    numbered IDs

--- public/app.js
+++ public/app.js
@@ -49,58 +49,58 @@ function sectionSelect(event) {
             let itemForm = document.createElement('div')
             itemForm.className = 'rounded border shadow mb-3 p-2 bg-white'
             itemForm.innerHTML = `
-                    <form>
+                    <form id="item-${iIndex}-form" data-item="${iIndex}" class="item-form">
                     <div class="form-group">
-                        <label for="itemname">Name: </label>
-                        <input type="text" name="name" id="itemname" class="w-100 form-control" value="${item.name || ""}">
+                        <label for="item-${iIndex}-name">Name: </label>
+                        <input type="text" name="name" id="item-${iIndex}-name" data-item="${iIndex}" class="w-100 form-control" value="${item.name || ""}">
                     </div>
 
                     <div class="form-group">
-                        <label for="itemdescription">Description: </label>
-                        <textarea name="description" id="itemdescription" rows="${item.description ? '2': '1'}" class="w-100 form-control">${item.description || ""}</textarea>
+                        <label for="item-${iIndex}-description">Description: </label>
+                        <textarea name="description" id="item-${iIndex}-description" data-item="${iIndex}" rows="${item.description ? '2': '1'}" class="w-100 form-control">${item.description || ""}</textarea>
                     </div>
 
                     <div class="form-group">
-                        <label for="itemprice">Image: </label>
-                        <input type="text" name="image" id="itemimage" class="w-100 form-control" value="${item.image || ""}">
+                        <label for="item-${iIndex}-price">Image: </label>
+                        <input type="text" name="image" id="item-${iIndex}-image" data-item="${iIndex}" class="w-100 form-control" value="${item.image || ""}">
                     </div>
 
                     <div class="form-group">
-                        <label for="itemprice">Price: </label>
-                        <input type="number" name="price" id="itemprice" class="w-100 form-control" value="${item.price || ""}">
+                        <label for="item-${iIndex}-price">Price: </label>
+                        <input type="number" name="price" id="item-${iIndex}-price" data-item="${iIndex}" class="w-100 form-control" value="${item.price || ""}">
                     </div>
 
                     <div class="form-group">
-                        <label for="itemextras">Extras: </label>
-                        <textarea name="extras" id="itemextras" rows="${item.extras ? '3' : '1'}" class="w-100 form-control">${item.extras || ""}</textarea>
+                        <label for="item-${iIndex}-extras">Extras: </label>
+                        <textarea name="extras" id="item-${iIndex}-extras" data-item="${iIndex}" rows="${item.extras ? '3' : '1'}" class="w-100 form-control">${item.extras || ""}</textarea>
                     </div>
 
                     <div class="form-group">
-                        <label for="itemoptions">Options: </label>
-                        <textarea name="options" id="itemoptions" rows="${item.options ? '3' : '1'}" class="w-100 form-control" >${item.options || ""}</textarea>
+                        <label for="item-${iIndex}-options">Options: </label>
+                        <textarea name="options" id="item-${iIndex}-options" data-item="${iIndex}" rows="${item.options ? '3' : '1'}" class="w-100 form-control" >${item.options || ""}</textarea>
 
                     </div>
                     <div class="row">
                         <div class="col-6">
                             <div class="form-group">
-                                <label for="itemfeatured">Featured: </label>
-                                <input type="checkbox" name="featured" id="itemfeatured" ${item.featured ? "checked" : ""} >
+                                <label for="item-${iIndex}-featured">Featured: </label>
+                                <input type="checkbox" name="featured" id="item-${iIndex}-featured" data-item="${iIndex}" ${item.featured ? "checked" : ""} >
                             </div>
 
                             <div class="form-group">
-                                <label for="itemglutenoption">Gluten Free Option: </label>
-                                <input type="checkbox" name="glutenoption" id="itemglutenoption" ${item["gf option"] ? "checked" : ""}>
+                                <label for="item-${iIndex}-glutenoption">Gluten Free Option: </label>
+                                <input type="checkbox" name="glutenoption" id="item-${iIndex}-glutenoption" data-item="${iIndex}" ${item["gf option"] ? "checked" : ""}>
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="form-group">
-                                <label for="itemvegan">Vegan: </label>
-                                <input type="checkbox" name="vegan" id="itemvegan" ${item.vegan ? "checked" : ""} >
+                                <label for="item-${iIndex}-vegan">Vegan: </label>
+                                <input type="checkbox" name="vegan" id="item-${iIndex}-vegan" data-item="${iIndex}" ${item.vegan ? "checked" : ""} >
                             </div>
 
                             <div class="form-group">
-                                <label for="itemvegetarian">Vegetarian: </label>
-                                <input type="checkbox" name="vegetarian" id="itemvegetarian" ${item.vegetarian ? "checked" : ""} >
+                                <label for="item-${iIndex}-vegetarian">Vegetarian: </label>
+                                <input type="checkbox" name="vegetarian" id="item-${iIndex}-vegetarian" data-item="${iIndex}" ${item.vegetarian ? "checked" : ""} >
                             </div>
                         </div>
                     </div>

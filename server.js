22feef87e6f9ad4875fa71e7e58dd17bf87a69b9
aOpenbrier <aopenbrier@gmail.com>
2019-07-17 19:30:03 -0700

    get menu from munchthai.com

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000
 const fs = require('fs')
+const axios = require('axios')
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -11,7 +12,18 @@ app.use(bodyParser.json())
 
 app.put('/menu', (req, res) => {
     res.sendStatus(200)
-    fs.writeFile('public/menu.json', JSON.stringify(req.body, null, 1), (e) => e && console.error(e))
+    fs.writeFile(path.join(__dirname, 'menu', 'menu.json'), JSON.stringify(req.body, null, 1), (e) => e && console.error(e))
+})
+
+app.get('/menu', (req, res) => {
+    res.sendFile(path.join(__dirname, "menu", "menu.json"))
+})
+
+app.get('/munchthaimenu', (req, res) => {
+    axios.get(`https://www.munchthai.com/assets/js/menu.json`)
+        .then(r => {
+            res.json(r.data)
+        }).catch(e => console.log(e))
 })
 
 app.listen(PORT, () => console.log(`listening at localhost:${PORT}`))
